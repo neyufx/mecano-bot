5485ab162923ed8c849634a472e846dd38cd424c
neyufx <neyufx@gmail.com>
2022-05-26 22:01:29 +0200

    v0.1

--- database/db.js
+++ database/db.js
@@ -4,9 +4,9 @@ require('dotenv').config();
     var pool = mysql.createPool({
         connectionLimit: 10,
         host: '127.0.0.1', // 153.92.220.151
-        user: 'root', // u463335117_brasserie
-        password: '', // Neyufx99&
-        database: 'bennys', // u463335117_brasserie
+        user: 'u463335117_mecano', // u463335117_brasserie
+        password: 'V[v2&x^8*:UN', // Neyufx99&
+        database: 'u463335117_mecano', // u463335117_brasserie
         connectTimeout: 30000
     })
     pool.getConnection(function(err, connection) {
