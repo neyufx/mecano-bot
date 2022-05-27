77707f73998a2028f2d813063932d4b323a1d902
neyufx <neyufx@gmail.com>
2022-05-27 13:49:16 +0200

    v0.1

--- commands/vire.js
+++ commands/vire.js
@@ -8,32 +8,25 @@ module.exports = {
     async execute(message,args){
         
         db.pool.getConnection(function(err, connection) {
+            if(args[0])
             var today = new Date();
             var dd = String(today.getDate()).padStart(2, '0');
             var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
             var yyyy = today.getFullYear();
             var date = yyyy + '/' + mm + '/' + dd;
             // Use the connection
-            if(args[0]){
-                let user = message.mentions.users.first()
-                let member = message.guild.members.cache.get(user.id);
-                let role = message.guild.roles.cache.get("976232655593037873"); // Rôle en attente
-                let role2 = message.guild.roles.cache.get("976232655605624882"); // Rôle intérimaire
-                let role3 = message.guild.roles.cache.get("976232655593037869"); // Rôle citoyen
-                connection.query(`UPDATE employee SET dateViree = "${date}" WHERE dossier = "${message.channel.name}"`, function (error, results, fields) {
-                // When done with the connection, release it.
-                message.channel.setParent('976232656322854936');
-                member.roles.remove(role); // Supprime le rôle en attente
-                member.roles.remove(role2); // Supprime le rôle en attente
-                member.roles.add(role3)
-                connection.release();
-                // Handle error after the release.
-                if (error) throw error;
-                // Don't use the connection here, it has been returned to the pool.
-                });
-            }else{
-                message.channel.send('Veuillez spécifier le @usertag.');
+            connection.query(`UPDATE employee SET dateViree = "${date}" WHERE dossier = "${message.channel.name}";
+                              UPDATE users SET isViree = "${date}" WHERE firstname = "${args[0]}"`, function (error, results, fields) {
+            // When done with the connection, release it.
+            message.channel.setParent('976232656322854936'); // Nom de la catégorie
+            connection.release();
+            // Handle error after the release.
+            if (error) throw error;
+            // Don't use the connection here, it has been returned to the pool.
+            else{
+                message.channel.send('Veuillez renseigné le prénom.');
             }
+            });
         });
 
         
